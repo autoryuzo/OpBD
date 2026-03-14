d5d971f83cf040212f6705affed69644b317d3cf
autoryuzo <trixsociety@mail.ru>
2026-03-14 14:03:14 +0300

    full reworked project, deleted our old scenaries

--- app/kafka/topics.py
+++
@@ -1,6 +0,0 @@
-DRONE_MISSION = "drone.mission"
-DRONE_TAKEOFF_REQUEST = "drone.takeoff_request"
-DRONE_TELEMETRY = "drone.telemetry"
-
-ORDV_TAKEOFF_AUTH = "ordv.takeoff_authorization"
-ORDV_EMERGENCY = "ordv.emergency"
